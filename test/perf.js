b28565015cc60f03efa087f14c9a08d54a48f5b4
John Yani <vanuan@gmail.com>
2013-02-08 23:40:58 +0200

    Fix test

--- test/perf.js
+++ test/perf.js
@@ -3,7 +3,7 @@ var geojson_api = require("../geojson-api");
 describe('geojson-api', function () {
   describe('#get_tile_data()', function(){
     it('should return in reasonable time', function () {
-      var wait = function (data) {}
+      var wait = { 'send': function() {}};
       for (var i = 0; i < 100; ++i) {
         for (var j = 0; j < 100; ++j) {
           geojson_api.get_tile_data('20/' + i + '/' + j, wait);
