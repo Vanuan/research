9450796847f08873aa3dba6743cf4c8821c9d070
John Yani <vanuan@gmail.com>
2013-02-09 18:20:55 +0200

    Proper perf test

---
+++ test/perf1.js
@@ -0,0 +1,11 @@
+var geojson_api = require("./geojson-api");
+var wait = { 'send': function(data) {}};
+var client = geojson_api.create_client();
+
+      for (var i = 0; i < 100; ++i) {
+        for (var j = 0; j < 100; ++j) {
+          geojson_api.get_tile_data('20/' + i + '/' + j, client, wait);
+        }
+      }
+
+geojson_api.close_client(client);
