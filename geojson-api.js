8564b0921244059cfb111d5a459f460065928a36
John Yani <vanuan@gmail.com>
2013-02-10 17:34:45 +0200

    Execute query

--- geojson-api.js
+++ geojson-api.js
@@ -115,7 +115,8 @@ function execute_query(prefix, tags, bounds, client, on_result) {
   var intscalefactor = 100;
   var query = prepare_polygon_query(prefix, tags, bbox, bounds.z, intscalefactor);
   client.query(query, function (err, result) {
-    on_query_result(err, result, on_result, bounds, intscalefactor);
+    on_result.send();
+    //on_query_result(err, result, on_result, bounds, intscalefactor);
   });
 }
 
@@ -255,21 +256,25 @@ function get_bounds(tile_id) {
 }
 
 function GrabData(tile_id, client, res){
+  var response  = res.response;
+  response.writeHead(200, res.headers);
   var bounds = get_bounds(tile_id);
   var z = parseInt(tile_id.split('/')[0]);
   var x = parseInt(tile_id.split('/')[1]);
   var y = parseInt(tile_id.split('/')[2]);
   var tags =['"addr:housenumber"', 'name', 'building', 'amenity'];
 //  execute_query(prefix, tags, bounds, client, res);
+  execute_query(prefix, tags, bounds, client, {
+    'send': function() {
+      var result = 'onKothicDataResponse({"features":[{"type":"Polygon","coordinates":[[[8.63,73.2],[30.51,164.21],[57.83,157.64],[35.94,66.63],[8.63,73.2]]],"properties":{"addr:housenumber":"148а/2","building":"yes"},"reprpoint":[[[8.63,73.2],[30.51,164.21],[57.83,157.64],[35.94,66.63],[8.63,73.2]]]},{"type":"Polygon","coordinates":[[[-15.37,-16.27],[6.99,63.12],[33.45,55.68],[11.08,-23.71],[-15.37,-16.27]]],"properties":{"addr:housenumber":"148а/1","building":"yes"},"reprpoint":[[[-15.37,-16.27],[6.99,63.12],[33.45,55.68],[11.08,-23.71],[-15.37,-16.27]]]}],"bbox":[30.7287597656,46.4033026733,30.7294464111,46.4037761667],"granularity":100},' + z + ',' + x + ',' + y +');'
+  response.write(result);
+  response.end();
+    }
+  });
 
-  var result = 'onKothicDataResponse({"features":[{"type":"Polygon","coordinates":[[[8.63,73.2],[30.51,164.21],[57.83,157.64],[35.94,66.63],[8.63,73.2]]],"properties":{"addr:housenumber":"148а/2","building":"yes"},"reprpoint":[[[8.63,73.2],[30.51,164.21],[57.83,157.64],[35.94,66.63],[8.63,73.2]]]},{"type":"Polygon","coordinates":[[[-15.37,-16.27],[6.99,63.12],[33.45,55.68],[11.08,-23.71],[-15.37,-16.27]]],"properties":{"addr:housenumber":"148а/1","building":"yes"},"reprpoint":[[[-15.37,-16.27],[6.99,63.12],[33.45,55.68],[11.08,-23.71],[-15.37,-16.27]]]}],"bbox":[30.7287597656,46.4033026733,30.7294464111,46.4037761667],"granularity":100},' + z + ',' + x + ',' + y +');'
 //  res.request = request;
 //  res.headers = headers;
 //  res.response = response;
-  var response  = res.response;
-  response.writeHead(200, res.headers);
-  response.write(result);
-  response.end();
 
 }
 
