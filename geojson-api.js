e9a23c2ab996f74dc89fd7434df8d78661eb4790
John Yani <vanuan@gmail.com>
2013-02-10 17:52:59 +0200

    complex query

--- geojson-api.js
+++ geojson-api.js
@@ -114,7 +114,9 @@ function execute_query(prefix, tags, bounds, client, on_result) {
   var bbox = bbox_to_projection(bounds.bounds, 'EPSG:900913');
   var intscalefactor = 100;
 //  var query = prepare_polygon_query(prefix, tags, bbox, bounds.z, intscalefactor);
-  var query = "ST_AsGeoJSON(way) FROM planet_osm_polygon;";
+//  var query = "ST_AsGeoJSON(way) FROM planet_osm_polygon;";
+  var query = "ST_AsGeoJSON((ST_TransScale(ST_ForceRHR((way)),20037393.687242553,-20033839.365384713,2.6165333200839087,2.6165262166577854)), 2) as way, ST_AsGeoJSON((ST_TransScale(ST_ForceRHR((way)),20037393.687242553,-20033839.365384713,2.6165333200839087,2.6165262166577854)), 2) as reprpoint, 'addr:housenumber',name,building,amenity FROM (SELECT (ST_Dump(ST_Multi(ST_SimplifyPreserveTopology(ST_Buffer(way,-2),2)))).geom as way, 'addr:housenumber',name,building,amenity FROM (SELECT ST_Union(way) as way, 'addr:housenumber',name,building,amenity FROM (SELECT ST_Buffer(way, 1) AS 'way', 'addr:housenumber',name,building,amenity FROM planet_osm_polygon WHERE (true AND way && SetSRID('BOX3D(-20037393.687242553 20033839.365384713,-20037355.46873441 20033877.583996613)'::box3d,900913) AND way_area > 0.5555555555555556) AND ('addr:housenumber' != '' OR name != '' OR building != '' OR amenity != '')) p GROUP BY 'addr:housenumber',name,building,amenity) as p) p;";
+
   client.query(query, function (err, result) {
     on_result.send();
     //on_query_result(err, result, on_result, bounds, intscalefactor);
